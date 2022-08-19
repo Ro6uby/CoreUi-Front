c07c23f4b863bc80cd61b98573417a14536bdadc
Ro6uby <101344066+Ro6uby@users.noreply.github.com>
2022-08-19 19:15:35 +0200

    Final version

---
+++ Only_front_version/app.js
@@ -0,0 +1,76 @@
+function show(){
+    let base = document.querySelector('.sous_base').style.display;
+
+    if (base == 'block'){
+        document.querySelector(".sous_base").style.display = "none";
+    }
+    else{
+        document.querySelector('.sous_base').style.display = "block";
+    }
+}
+
+function show2(){
+    let button = document.querySelector('.sous_button').style.display;
+
+    if (button == 'block'){
+        document.querySelector(".sous_button").style.display = "none";
+    }
+    else{
+        document.querySelector('.sous_button').style.display = "block";
+    }
+}
+
+
+function show3(){
+    let forms = document.querySelector('.sous_form').style.display;
+
+    if (forms == 'block'){
+        document.querySelector(".sous_form").style.display = "none";
+    }
+    else{
+        document.querySelector('.sous_form').style.display = "block";
+    }
+}
+
+
+
+function show4(){
+    
+    let icons = document.querySelector('.sous_icon').style.display;
+
+    if (icons == 'block'){
+        document.querySelector(".sous_icon").style.display = "none";
+    }
+    else{
+        document.querySelector('.sous_icon').style.display = "block";
+    }
+}
+
+
+function show5(){
+    let notification = document.querySelector('.sous_notif').style.display;
+    
+        if (notification == 'block'){
+            document.querySelector(".sous_notif").style.display = "none";
+        }
+        else{
+            document.querySelector('.sous_notif').style.display = "block";
+        }
+}
+
+
+
+function show6(){
+    let pages = document.querySelector('.sous_page').style.display;
+    
+    if (pages == 'block'){
+        document.querySelector(".sous_page").style.display = "none";
+    }
+    else{
+        document.querySelector('.sous_page').style.display = "block";
+    }
+}
+ 
+
+
+    
